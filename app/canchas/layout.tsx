5eb73a46c20ee63d3bb3d11c9d46b00f7c708924
ulicortes <ulisestcortes@gmail.com>
2024-01-12 16:27:35 -0300

    pestaña de reservar hecha por arriba, actualizacion de bbdd.

--- app/canchas/layout.tsx
+++ app/canchas/layout.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div>
+      <div className="pl-4 pt-2">
         <Link
           href="/"
-          className="group rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          
           rel="noopener noreferrer"
         >
-          <h3 className={`p-3 text-2xl text-center font-semibold`}>
+          <h5 className={`p-3. text-l text-left. ml-2. font-semibold`}>
             Volver
-          </h3>
+          </h5>
         </Link>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-2. md:overflow-y-auto md:p-2">{children}</div>
     </div>
   );
 }
--- app/canchas/reservar/page.tsx
+++ app/canchas/reservar/page.tsx
@@ -1,68 +1,82 @@
 export default function Page() {
     return <div>
         <div>
-            <form action="" method="post" className="m-auto">
-                <div className="pb-12 text-center">
-                    <h2 className="text-base font-semibold leading-7 text-gray-900">Reservar cancha</h2>
+            <form action="" method="" className="m-auto">
+                <div className="pt-12 pb-12. text-center">
+                    <h1 className="text-base text-3xl font-bold leading-7 text-gray-900">Reservar cancha</h1>
                     <p className="mt-1 text-sm leading-6 text-gray-600">Completa los datos de la cancha</p>
 
-                    <div className="mt-2. grid grid-cols-1 gap-x-10 gap-y-8 sm:grid-cols-2 w-3/5 m-auto">
-                        <div className="sm:col-span-1 sm:col-start-1">
+                    <div className="mt-10 grid grid-cols-1 gap-x-10 gap-y-8 sm:grid-cols-4 w-3/5 m-auto">
+
+                        <div className="sm:col-span-2 sm:col-start-2.">
                             <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
                                 Organizador
                             </label>
                             <div className="mt-2">
                                 <input
                                     type="text"
-                                    name="city"
-                                    id="city"
+                                    name="org"
+                                    id="org"
                                     autoComplete="address-level2"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="text-center block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
 
-                        <div className="sm:col-span-1">
+                        <div className="sm:col-span-2">
                             <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
                                 Numero de telefono
                             </label>
                             <div className="mt-2">
                                 <input
                                     type="number"
-                                    name="region"
-                                    id="region"
-                                    autoComplete="address-level1"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    name="tel"
+                                    id="tel"
+                                    className="text-center block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
 
-                        <div className="sm:col-span-1">
+                        <div className="sm:col-span-2">
                             <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
                                 Lugar
                             </label>
                             <div className="mt-2">
                                 <input
                                     type="text"
-                                    name="first-name"
-                                    id="first-name"
+                                    name="lugar"
+                                    id="lugar"
                                     autoComplete="given-name"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="text-center block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
 
-                        <div className="sm:col-span-1">
+                        <div className="sm:col-span-2">
                             <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
                                 Direccion
                             </label>
                             <div className="mt-2">
                                 <input
                                     type="text"
-                                    name="last-name"
-                                    id="last-name"
+                                    name="direcc"
+                                    id="direcc"
                                     autoComplete="family-name"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="text-center block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-1">
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Dia
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="dia"
+                                    name="dia"
+                                    type="date"
+                                    className="text-center block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -73,8 +87,8 @@ export default function Page() {
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="email"
-                                    name="email"
+                                    id="hora"
+                                    name="hora"
                                     type="time"
                                     autoComplete="email"
                                     className="text-center block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -86,11 +100,10 @@ export default function Page() {
                             <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                                 Cantidad de jugadores
                             </label>
-                            <div className="mt-2 ml-6">
+                            <div className="mt-3">
                                 <select
-                                    id="country"
-                                    name="country"
-                                    autoComplete="country-name"
+                                    id="jugs"
+                                    name="jugs"
                                     className="text-center block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-3"
                                 >
                                     <option>10</option>
@@ -101,10 +114,10 @@ export default function Page() {
                             </div>
                         </div>
 
-                        <div className="sm:col-span-2">
+                        <div className="sm:col-span-1 self-end">
                             <button
                                 type="submit"
-                                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Reservar
                             </button>
