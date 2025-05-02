7d4bd1ae5498bc8098100a36600af2a324ae19e0
ulicortes <74629456+ulicortes@users.noreply.github.com>
2025-05-02 11:52:48 -0300

    Update metodos.ts v6

--- app/lib/metodos.ts
+++ app/lib/metodos.ts
@@ -89,8 +89,8 @@ export async function ingresarUsuario(formData: FormData) {
   if (rsp.status == 200) {
     let data = await rsp.json();
     await setCookies(data.token);
-    revalidatePath(`/${ulr}`);
-    redirect(`/${ulr}`);
+    revalidatePath(`${baseUrl}/${ulr}`);
+    redirect(`${baseUrl}/${ulr}`);
   }
   else throw new Error('1');
   // .then(rsp => {
